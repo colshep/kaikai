ac07cce3517ae1928d7a03d13254178e51cc4740
colshep <colshep@163.com>
2021-05-27 09:36:29 +0800

    init

---
+++ src/main/java/com/plunger/kaikai/KaikaiApplication.java
@@ -0,0 +1,13 @@
+package com.plunger.kaikai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaikaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaikaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plunger/kaikai/KaikaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plunger.kaikai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaikaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
